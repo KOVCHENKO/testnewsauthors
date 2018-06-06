c74cf8f276e0e9b2c9d65db932946507d13bdb0b
il_kow <il_kow@localhost.localdomain>
2018-06-06 10:46:37 +0400

    Добавлены миграции и контроллер для новостей

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class NewsController extends Controller
+{
+    public function getNews()
+    {
+
+    }
+
+    public function getById($id)
+    {
+
+    }
+
+    public function getByFilter($parameters)
+    {
+
+    }
+}
